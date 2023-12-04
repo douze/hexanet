8ad932785b1979d6050c43c1b2b68d3afa018df5
douze <douze@users.noreply.github.com>
2023-12-04 17:39:13 +0100

    Isocahedron generation optimization (reuse main triangle vertices, cache midpoint index)

--- icosahedron/icosahedron.gd
+++ icosahedron/icosahedron.gd
@@ -16,88 +16,109 @@ extends MeshInstance3D
 		subdivisions = value
 		_generate_mesh()
 
+var _vertices := PackedVector3Array()
+var _indices := PackedInt32Array()
 
-func _ready():
+
+func _ready() -> void:
 	_generate_mesh()
 
 
-func _generate_mesh():
-	var surface_array: Array = []
-	surface_array.resize(Mesh.ARRAY_MAX)
+# Generate basic isocahedron shape
+# https://en.wikipedia.org/wiki/Regular_icosahedron
+# http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
+func _create_basic_icosahedron() -> void:
+	var golden_ratio := (1+sqrt(5))/2; 
+	
+	_vertices.append(Vector3(-1, golden_ratio, 0))
+	_vertices.append(Vector3(1, golden_ratio, 0,))
+	_vertices.append(Vector3(-1, -golden_ratio, 0))
+	_vertices.append(Vector3(1, -golden_ratio, 0))
+	_vertices.append(Vector3(0, -1, golden_ratio))
+	_vertices.append(Vector3(0, 1, golden_ratio))
+	_vertices.append(Vector3(0, -1, -golden_ratio))
+	_vertices.append(Vector3(0, 1, -golden_ratio))
+	_vertices.append(Vector3(golden_ratio, 0, -1))
+	_vertices.append(Vector3(golden_ratio, 0, 1))
+	_vertices.append(Vector3(-golden_ratio, 0, -1))
+	_vertices.append(Vector3(-golden_ratio, 0, 1))
+	
+	_indices.append_array([
+		5, 11, 0,	1, 5, 0,	7, 1, 0,	10, 7, 0,	11, 10, 0,
+		9, 5, 1,	4, 11, 5,	2, 10, 11,	6, 7, 10,	8, 1, 7,
+		4, 9, 3,	2, 4, 3,	6, 2, 3,	8, 6, 3,	9, 8, 3,
+		5, 9, 4,	11, 4, 2,	10, 2, 6,	7, 6, 8,	1, 8, 9
+	])
 
-	# Generate basic isocahedron shape
-	# https://en.wikipedia.org/wiki/Regular_icosahedron
-	# http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
-	
-	var golden_ratio = (1+sqrt(5))/2; 
-	
-	var vertices = PackedVector3Array()
-	vertices.append(Vector3(-1, golden_ratio, 0))
-	vertices.append(Vector3(1, golden_ratio, 0,))
-	vertices.append(Vector3(-1, -golden_ratio, 0))
-	vertices.append(Vector3(1, -golden_ratio, 0))
-	vertices.append(Vector3(0, -1, golden_ratio))
-	vertices.append(Vector3(0, 1, golden_ratio))
-	vertices.append(Vector3(0, -1, -golden_ratio))
-	vertices.append(Vector3(0, 1, -golden_ratio))
-	vertices.append(Vector3(golden_ratio, 0, -1))
-	vertices.append(Vector3(golden_ratio, 0, 1))
-	vertices.append(Vector3(-golden_ratio, 0, -1))
-	vertices.append(Vector3(-golden_ratio, 0, 1))
-	
-	var indices = PackedInt32Array()
-	indices.append_array([
-			5, 11, 0,	1, 5, 0,	7, 1, 0,	10, 7, 0,	11, 10, 0,
-			9, 5, 1,	4, 11, 5,	2, 10, 11,	6, 7, 10,	8, 1, 7,
-			4, 9, 3,	2, 4, 3,	6, 2, 3,	8, 6, 3,	9, 8, 3,
-			5, 9, 4,	11, 4, 2,	10, 2, 6,	7, 6, 8,	1, 8, 9
-		])
-		
-	# Subdivide
+
+# Get the index of the segment midpoint
+# Value is cached because midpoint is shared between multiple triangles
+func _get_midpoint_index(first_point: Vector3, second_point: Vector3, segment_midpoint_cache: Dictionary) -> int:
+	var midpoint: Vector3 = (first_point + second_point) / 2
+	var dic_key := [first_point, second_point]
+	var index_midpoint = segment_midpoint_cache.get(dic_key)
+	if (index_midpoint == null):
+		_vertices.append(midpoint)
+		index_midpoint = _vertices.size() - 1
+		segment_midpoint_cache[dic_key] = index_midpoint
+	return index_midpoint
+
+
+# Subdivide the main geometry using loop subdivisions
+func _subdivide() -> void:
+	var segment_midpoint_cache := Dictionary()
 	for i in range(subdivisions):
-		var vertices2 = PackedVector3Array()
-		var indices2 = PackedInt32Array()
-		
-		var index = 0
-		for j in range(0, indices.size(), 3):
-			# Grab triangle
-			var a = vertices[indices[j]]
-			var b = vertices[indices[j+1]]
-			var c = vertices[indices[j+2]]
-			# Compute mid points for each segments
-			var ab = (a+b)/2
-			var bc = (b+c)/2
-			var ca = (c+a)/2
-			# Create 6 vertices for each triangle
-			vertices2.append(a)
-			vertices2.append(ab)
-			vertices2.append(b)
-			vertices2.append(bc)
-			vertices2.append(c)
-			vertices2.append(ca)
-			# From original triangle, create 4 triangles
-			indices2.append(index)
-			indices2.append(index+1)
-			indices2.append(index+5)
-			indices2.append(index+1)
-			indices2.append(index+2)
-			indices2.append(index+3)
-			indices2.append(index+3)
-			indices2.append(index+4)
-			indices2.append(index+5)
-			indices2.append(index+1)
-			indices2.append(index+3)
-			indices2.append(index+5)
-			index += 6
-		vertices = vertices2
-		indices = indices2
-		
-	# Apply radius
-	for i in range(vertices.size()):
-		vertices[i] = vertices[i].normalized() * radius
-	
-	# Create mesh
-	surface_array[Mesh.ARRAY_VERTEX] = vertices
-	surface_array[Mesh.ARRAY_INDEX] = indices
+		var sub_indices := PackedInt32Array()
+		for j in range(0, _indices.size(), 3):
+			# Grab triangle ABC
+			var idx_a = _indices[j]
+			var idx_b = _indices[j+1]
+			var idx_c = _indices[j+2]
+			var a := _vertices[idx_a]
+			var b := _vertices[idx_b]
+			var c := _vertices[idx_c]
+			# Get midpoint for each segment of the ABC triangle
+			var idx_ab = _get_midpoint_index(a,b,segment_midpoint_cache)
+			var idx_bc = _get_midpoint_index(b,c,segment_midpoint_cache)
+			var idx_ca = _get_midpoint_index(c,a,segment_midpoint_cache)
+			# From original triangle ABC, create 4 sub triangles
+			sub_indices.append(idx_a)
+			sub_indices.append(idx_ab)
+			sub_indices.append(idx_ca)
+			sub_indices.append(idx_ab)
+			sub_indices.append(idx_b)
+			sub_indices.append(idx_bc)
+			sub_indices.append(idx_ca)
+			sub_indices.append(idx_bc)
+			sub_indices.append(idx_c)
+			sub_indices.append(idx_ab)
+			sub_indices.append(idx_bc)
+			sub_indices.append(idx_ca)
+		# Replace main indices by sub indices for loop subdivisions
+		_indices = sub_indices
+
+
+# Apply radius to the geometry
+func _apply_radius() -> void:
+	for i in range(_vertices.size()):
+		_vertices[i] = _vertices[i].normalized() * radius
+
+
+# Generate the icosahedron mesh
+func _generate_mesh() -> void:
+	_vertices.clear()
+	_indices.clear()
 	mesh.clear_surfaces()
+	
+	_create_basic_icosahedron()
+	_subdivide()
+	_apply_radius()
+	
+	var surface_array := Array()
+	surface_array.resize(Mesh.ARRAY_MAX)
+	surface_array[Mesh.ARRAY_VERTEX] = _vertices
+	surface_array[Mesh.ARRAY_INDEX] = _indices
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	
+	_vertices.clear()
+	_indices.clear()
